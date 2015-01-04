9e913ec2540b655199f9ff6850ad11c452d86ece
zhouzhuojie <zhouzhuojie@gmail.com>
2015-01-04 15:33:31 -0500

    Update upstream

--- package.js
+++ package.js
@@ -1,25 +1,18 @@
 Package.describe({
   summary: "Javascript-state-machine for Meteor",
-  version: "0.1.0",
+  version: "0.1.1",
   git: "https://github.com/zhouzhuojie/meteor-state-machine.git",
   name: "mrt:state-machine"
 });
 
-Package.on_use(function (api) {
+Package.onUse(function (api) {
+  api.versionsFrom('1.0');
   api.add_files('javascript-state-machine/state-machine.min.js', 'client');
   api.add_files('main.js', 'client');
-  if (api.export){
-    api.export('StateMachine', 'client');
-  }
+  api.export('StateMachine', 'client');
 });
 
-if (Package.on_test) {
-  Package.on_test(function (api) {
-    if (Package.onTest) {
-      api.use(['mrt:state-machine', 'tinytest', 'test-helpers'], ['client']);
-    } else {
-      api.use(['state-machine', 'tinytest', 'test-helpers'], ['client']);
-    }
-    api.add_files('test-mrt:state-machine.js', ['client']);
-  });
-}
+Package.onTest(function (api) {
+  api.use(['mrt:state-machine', 'tinytest'], ['client']);
+  api.add_files('test-mrt:state-machine.js', ['client']);
+});
